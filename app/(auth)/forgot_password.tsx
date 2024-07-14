5d1947ffbbe69a032c905b94d838eb76cb284d3f
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-07-14 15:55:13 +0100

    fixed password

--- app/(auth)/forgot_password.tsx
+++ app/(auth)/forgot_password.tsx
@@ -3,25 +3,36 @@ import CustomButton from "@/components/button";
 import CustomPhoneNumber from "@/components/phoneInput";
 import { CustomText } from "@/components/text";
 import { Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import * as Yup from "yup";
 
 const ForgotPassword = () => {
+  const [currentStep, setCurrentStep] = useState(1);
   const validationSchema = Yup.object({
     phone: Yup.string().required("Phone number is required"),
-    password: Yup.string()
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-8])(?=.*[!@#$%^&*:;'><.,/?}{[\]\-_+=])(?=.{8,})/,
-        "Must Contain 7 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-      )
-      .required("Password is required"),
   });
 
   const onSubmit = async (values: Record<string, any>, { resetForm }: any) => {
-    console.log("values", values);
+    console.log("values -- ", values);
+    setCurrentStep(2);
     resetForm({});
   };
+
+  if (currentStep === 2) {
+    return (
+      <AuthWrapper>
+        <View className="shadow-fuchsia-900 border-gray-500 border-1 justify-center h-[40vh]">
+          <CustomText
+            customClassName="text-[#979797]"
+            fontFamily="PoppinsMedium"
+          >
+            Change Your Phone Number
+          </CustomText>
+        </View>
+      </AuthWrapper>
+    );
+  }
 
   return (
     <AuthWrapper>
@@ -32,7 +43,7 @@ const ForgotPassword = () => {
 
         <Formik
           initialValues={{
-            password: "",
+            phone: "",
           }}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
